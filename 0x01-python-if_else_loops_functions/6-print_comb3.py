38a4d82dce885ced4381061cd2d0baa6fb00216d
tech7895 <techhunter990@gmail.com>
2023-10-31 03:41:10 -0700

    Inventing is a combination of brains and materials

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""Prints all possible different combinations of two digits in ascending order"""
+"""Prints all possible different combinations of two digits
+in ascending order"""
 
 for dgt1 in range(0, 10):
     for dgt2 in range(dgt1 + 1, 10):
