767c51d85028e4fb2c03d82a0d5f9c040fc6d297
tech7895 <techhunter990@gmail.com>
2023-11-21 01:17:31 -0800

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python3
 
-for k in range(ord('a'), ord('z') + 1):
-    if chr(k) != 'e' and chr(k) != 'q':
-        print('{:c}'.format(k), end='')
+# 0-square.py by Odoi Leonard
+"""This is a module that defines a square """
+
+
+class Square:
+
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: This represnets the size of the square defined
+        Raises:
+            TypeError: size is not integer
+            ValueError: the size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """
+        This calculates the area of the square
+        Returns: the size
+        """
+
+        return (self.__size ** 2)
