e39bcf093287e75360fec6e0f3329c9d297c9292
tech7895 <techhunter990@gmail.com>
2023-11-27 05:01:08 -0800

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This is a class that defines a rectangle"""
+
+
+class Rectangle:
+    """empty class that represents a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+This Directory contains the project:
+-----------------------------------
+0x08-python-more_classes
