ca179f5ff684852dce30d2bc967282150492d506
tech7895 <coper5540@gmail.com>
2023-12-04 15:17:26 +0000

    Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""this module defines a Rectangle
+the subclass Square"""
+Rectangle = __import__('9-rectangle').Rectangle
 
-import sys
 
+class Square(Rectangle):
+    """Represent a square"""
 
-def safe_print_integer_err(value):
-    try:
-        print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (False)
+    def __init__(self, size):
+        """This initialize a new square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
