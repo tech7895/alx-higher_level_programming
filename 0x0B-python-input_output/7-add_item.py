4f42daf688e50d2c187fae279e09564483e48e9f
tech7895 <coper5540@gmail.com>
2023-12-05 09:29:56 +0000

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-"""This module adds all arguments to a Python list andsave
+"""Add all arguments to a Python list and save
 them to a file."""
-
-
 import sys
 
 if __name__ == "__main__":
