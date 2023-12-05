eccba352213056fc46f216cd4fd5c020e986ba7b
tech7895 <coper5540@gmail.com>
2023-12-05 10:47:07 +0000

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
 
-"""
- save items to a file
-"""
 
-import sys
-import os.path
+import json
 
-args = sys.argv[1:]
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-item = []
-if os.path.exists("./add_item.json"):
-    item = load_from_json_file("add_item.json")
-
-save_to_json_file(item + args, "add_item.json")
+def save_to_json_file(my_obj, filename):
+    with open(filename, "w", encoding="UTF-8") as f:
+        json.dump(my_obj, f)
