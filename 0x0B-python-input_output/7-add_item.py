6ab0d58ec4816f6b6b25d8f46d00c7b9285a4d5c
tech7895 <coper5540@gmail.com>
2023-12-05 09:14:36 +0000

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""
+This module  saves items to a file
+"""
 
-"""This defines a JSON file-writing function"""
-import json
+import sys
+import os.path
 
+args = sys.argv[1:]
 
-def save_to_json_file(my_obj, filename):
-    """This writes an object to a text file using JSON representation"""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+item = []
+if os.path.exists("./add_item.json"):
+    item = load_from_json_file("add_item.json")
+
+save_to_json_file(item + args, "add_item.json")
