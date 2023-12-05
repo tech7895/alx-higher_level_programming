680a2f184fa1b635d13dec0720a98f6757508f2b
tech7895 <coper5540@gmail.com>
2023-12-05 11:15:02 +0000

    4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-"""Module containing inherits_from method"""
+"""Module containing the function from_json_string"""
+import json
 
 
-def inherits_from(obj, a_class):
-    """returns True if the object is an instance of a class that
-    inherited (directly or indirectly) from the specified class;
-    otherwise False"""
-    return isinstance(obj, a_class) and type(obj) != a_class
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string.
+
+    Args:
+        my_str (str): json object to convert to Python object.
+
+    Returns:
+        type: Python object.
+    """
+    # print("type json.loads(my_str)--> {}".format(type(json.loads(my_str))))
+    # print("type my_str--> {}".format(type(my_str)))
+    return json.loads(my_str)
