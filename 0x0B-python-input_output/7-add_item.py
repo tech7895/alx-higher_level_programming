7790b11b8313088f1430d0b6546607b54736e2c5
tech7895 <coper5540@gmail.com>
2023-12-05 11:23:26 +0000

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+"""Module that loads, adds and saves arguments to a Python list"""
+from sys import argv
 
-"""Defines a JSON file-writing function."""
-import json
 
+load_file = __import__('6-load_from_json_file').load_from_json_file
+save_file = __import__('5-save_to_json_file').save_to_json_file
 
-def save_to_json_file(my_obj, filename):
-    """Write an object to a text file using JSON representation."""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
+try:
+    json_list = load_file('add_item.json')
+except (ValueError, FileNotFoundError):
+    json_list = []
+
+for item in argv[1:]:
+    json_list.append(item)
+
+save_file(json_list, 'add_item.json')
