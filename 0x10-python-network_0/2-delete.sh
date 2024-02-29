e79d239ca98e831f08419939a665139eb9e40b5b
tech7895 <techhunter990@gmail.com>
2024-02-29 02:12:52 -0800

    Task 0 to 4

---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# script that sends a DELETE request to the URL passed as thefirst argument and
+# displays the body of the response
+curl -s "$1" -X DELETE
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,1 @@
+Project 0x10-python-network_0
