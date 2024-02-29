c8e855d66fba13df20988be54b09b4489481aed2
tech7895 <techhunter990@gmail.com>
2024-02-29 04:18:53 -0800

    updated files

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# The script makes a request to causes an specific response
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 """Algorithms for list of integers."""
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers."""
+    """This finds a peak in a list of unsorted integers."""
     if list_of_integers:
         list_of_integers.sort(reverse=True)
         return list_of_integers[0]
